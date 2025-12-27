1279cae21cd3da51f5b4894c44f52c4b97c4e74c
Michal Klik <1456289+TroodoNmike@users.noreply.github.com>
2025-12-27 20:58:31 +0100

    add d2 svg file type

---
+++ src/main/kotlin/com/troodon/d2/icons/D2Icons.kt
@@ -0,0 +1,9 @@
+package com.troodon.d2.icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object D2Icons {
+    @JvmField
+    val FILE: Icon = IconLoader.getIcon("/icons/d2File.svg", D2Icons::class.java)
+}
--- src/main/kotlin/com/troodon/d2/lang/D2FileType.kt
+++ src/main/kotlin/com/troodon/d2/lang/D2FileType.kt
@@ -1,10 +1,11 @@
 package com.troodon.d2.lang
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.troodon.d2.icons.D2Icons
 
 class D2FileType : LanguageFileType(D2Language.INSTANCE) {
     override fun getName() = "D2"
     override fun getDescription() = "D2 diagram file"
     override fun getDefaultExtension() = "d2"
-    override fun getIcon() = null
+    override fun getIcon() = D2Icons.FILE
 }
